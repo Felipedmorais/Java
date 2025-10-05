6777c2470274f0564baaae3b966c0e35c8436c17
felipekmorais@gmail.com <felipekmorais@gmail.com>
2025-10-05 00:23:17 -0300

    Fazendo testes matrizes

--- Main.java
+++ Main.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
+                System.out.println("Digite o tamanho da sua matriz: ");
+                int n = sc.nextInt();
+
+                int[][] matriz = new int[n][n];
+
+                for (int i = 0; i < n; i++){
+                        for (int j = 0;j < n; j++){
+                                System.out.println("Digite o valor da posição " + i + " " + j);
+                                matriz[i][j] = sc.nextInt();
+                        }
+                }
+                System.out.println("----------------------------------");
+                System.out.println();
+                number1 m1 =  new number1(matriz);
+                m1.exibirmatriz(matriz, n);
 
                 sc.close();
         }
--- number1.java
+++ number1.java
@@ -1,2 +1,20 @@
 public class number1 {
+    private int[][] matriz;
+    private int tamanho;
+
+    public number1(int[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public void exibirmatriz(int[][] matriz, int n) {
+        System.out.println("Matriz digitada:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
