821f333ad2fe29dfc26d6fcf91f101166ef64bba
felipekmorais@gmail.com <felipekmorais@gmail.com>
2025-10-12 16:51:29 -0300

    Testando o Enum

--- Main.java
+++ Main.java
@@ -2,15 +2,22 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
-                number1 motor = new number1(120);
-                number2 Carro = new number2(motor);
+                number1 order = new number1(1080,new Date(), Order.PedidoPago);
 
-                Carro.ligarCarro();
+                if (order.getStatus().equals(Order.PedidoPago)){
+                        order.setStatus(Order.Processando);
+                        if (order.getStatus().equals(Order.Processando)){
+                                order.setStatus(Order.Entregue);
+                        }
+                }
+
+                System.out.println(order);
 
         }
 }
--- number1.java
+++ number1.java
@@ -1,11 +1,50 @@
-public class number1 {
-    private int potencia;
+import javax.xml.crypto.Data;
+import java.util.Date;
 
-    public number1(int potencia) {
-        this.potencia = potencia;
+public class number1 {
+    private Integer id;
+    private Date moment;
+    private Order Status;
+
+    @Override
+    public String toString() {
+        return "number1{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", Status=" + Status +
+                '}';
     }
 
-    public void ligar(){
-        System.out.println("Motor ligado! ha " + potencia);
+    public number1(){
+
+    }
+    public number1(Integer id, Date moment, Order status) {
+        this.id = id;
+        this.moment = moment;
+        Status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public Order getStatus() {
+        return Status;
+    }
+
+    public void setStatus(Order status) {
+        Status = status;
     }
 }
